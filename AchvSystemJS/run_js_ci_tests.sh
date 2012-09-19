00e467a2c9efde3b1f5a275ac682628ea831fef1
Tim Kröner <t.kroener@tarent.de>
2012-09-19 15:52:46 +0200

    Changed from firefox to iceweasel.

--- AchvSystemJS/run_js_ci_tests.sh
+++ AchvSystemJS/run_js_ci_tests.sh
@@ -9,19 +9,19 @@ then
     exit 1
 fi
 
-FIREFOX=`which firefox`
+FIREFOX=`which iceweasel`
 if [ "$?" -eq 1 ];
 then
     echo "Firefox not found."
     exit 1
 fi
 
-# $XVFB :99 -ac &    # launch virtual framebuffer into the background
-# PID_XVFB="$!"      # take the process ID
-# export DISPLAY=:99 # set display to use that of the xvfb
+$XVFB :99 -ac &    # launch virtual framebuffer into the background
+PID_XVFB="$!"      # take the process ID
+export DISPLAY=:99 # set display to use that of the xvfb
 
 # run the tests
 java -jar JsTestDriver/JsTestDriver-1.3.4.b.jar --config AchvSystemJS/jsTestDriver.conf --port 9876 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
 
-# kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
+kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
 echo "Done."
