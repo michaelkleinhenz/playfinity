40b8e35a94ea98afef9062de0013a561b2495748
Tim Kröner <t.kroener@tarent.de>
2012-09-26 17:32:19 +0200

    Inserting achievements in store.
    Added inital functionality for insert and read achievements from
    database. Introduce nano module for wrapping couch db calls.

--- AchvSystemJS/src/store/Store.js
+++ AchvSystemJS/src/store/Store.js
@@ -1,15 +1,40 @@
 var express = require('express');
-var winston = require('winston');
+var logger = require('winston');
+var nano = require('nano')('http://localhost:5984');
+
+var db_name = "achievement";
+var db = nano.use(db_name);
 
 var app = module.exports = express();
 
 function readAchievement(req, res, next) {
-    winston.info('readAchievement()');
-    res.json(200, {"name":"myAchievement"})
+    logger.info('readAchievement()');
+    db.get("myAchievement", function(err, body) {
+        if(!err) {
+            res.json(200, body);
+        } else {
+            res.send(404);
+        }
+    });
 }
 
 function createAchievement(req, res, next) {
-    winston.info('createAchievement()');
+    logger.info('createAchievement()');
+    var doc = {"name": "myAchievement"};
+    db.insert(doc, doc.name, function(error, http_body, http_headers) {
+        if (error) {
+            if(error.message === 'no_db_file') {
+                return nano.db.create(db_name, function() {
+                    db.insert(doc, doc.name, function(error, http_body, http_headers){
+                       return logger.error("Not able to insert " + doc + "Reason: " + error);
+                    });
+                })
+            } else {
+                return logger.error("Not able to insert " + doc + " Reason: " + error);
+            }
+        }
+        logger.debug(JSON.stringify(http_body));
+    });
     res.status(204);
 	res.send();
 }
