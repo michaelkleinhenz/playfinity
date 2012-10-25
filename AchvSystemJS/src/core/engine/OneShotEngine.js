7e5e5aabc72c4d42cee1168f8a5affc3159d0d12
Tim Kröner <t.kroener@tarent.de>
2012-10-25 16:50:19 +0200

    Fixed some JsLint issues.

--- AchvSystemJS/src/core/engine/OneShotEngine.js
+++ AchvSystemJS/src/core/engine/OneShotEngine.js
@@ -1,17 +1,20 @@
 /*global ACHV*/
-ACHV.OneShotEngine = function () {
-    this.achievementType = "OneShotRule";
-};
+(function () {
+    "use strict";
+    ACHV.OneShotEngine = function () {
+        this.achievementType = "OneShotRule";
+    };
 
-ACHV.OneShotEngine.prototype.process = function (event, rule, valueChanged) {
-    if (rule.event === event.name) {
-        rule.state = "satisfied";
-        valueChanged(true);
-    }
-};
+    ACHV.OneShotEngine.prototype.process = function (event, rule, valueChanged) {
+        if (rule.event === event.name) {
+            rule.state = "satisfied";
+            valueChanged(true);
+        }
+    };
 
-ACHV.OneShotEngine.prototype.reset = function(rule) {
-    rule.state = "inProgress";
-};
+    ACHV.OneShotEngine.prototype.reset = function (rule) {
+        rule.state = "inProgress";
+    };
+}());
 
 exports.OneShotEngine = ACHV.OneShotEngine;
