7b93756811b0bce74eebf102a7f4b92a03ca93eb
Tim Kröner <t.kroener@tarent.de>
2012-10-31 14:49:27 +0100

    Added tests for AchievementInstanceStore.

--- AchvSystemJS/test/store/AchievementInstanceStoreTest.js
+++ AchvSystemJS/test/store/AchievementInstanceStoreTest.js
@@ -1,10 +1,10 @@
 /*global ACHV*/
 TestCase("AchievemenInstanceStoreTest", {
-    testCreateOrUpdateAchievementInstanceError: function () {
+    setUp: function () {
         "use strict";
-
         var db = {
-                insert: function () {}
+                insert: function () {},
+                destroy: function (docName, revision, callback) {}
             },
             dbMock = mock(db),
             logger = {
@@ -13,18 +13,51 @@ TestCase("AchievemenInstanceStoreTest", {
             conf = {
                 "db": dbMock,
                 "logger": logger
-            },
-            store = ACHV.achievementInstanceStore(conf);
+            };
 
         when(dbMock).insert(anything()).
             then(function (doc, docName, callback) {
                 callback("SomeError", null);
             });
 
-        store.createOrUpdateAchievementInstance("doc", createOrUpdateAchvInsCallback);
+        when(dbMock).destroy(anything()).
+            then(function (docName, revision, callback) {
+                if (revision === "error_revision") {
+                    callback("DestroyError", null);
+                } else {
+                    callback(null, "Body");
+                }
+            });
+
+        this.store = ACHV.achievementInstanceStore(conf);
+    },
+
+    testCreateOrUpdateAchievementInstanceError: function () {
+        "use strict";
+        this.store.createOrUpdateAchievementInstance("doc", createOrUpdateAchvInsCallback);
 
         function createOrUpdateAchvInsCallback(error, result) {
             assertEquals("SomeError", error);
+            assertNull(result);
+        }
+    },
+
+    testDeleteAchievement: function () {
+        "use strict";
+        this.store.deleteAchievement("docname", "revision", deleteAchievementCallback);
+
+        function deleteAchievementCallback(error, result) {
+            assertNull(error);
+            assertEquals("Body", result);
+        }
+    },
+
+    testDeleteAchievementError: function () {
+        "use strict";
+        this.store.deleteAchievement("docname", "error_revision", deleteAchievementCallback);
+
+        function deleteAchievementCallback(error, result) {
+            assertEquals("DestroyError", error);
             assertNull(result);
         }
     }
